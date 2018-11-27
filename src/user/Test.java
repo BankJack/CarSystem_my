b24a9c80550ba7e038b35a42d3d0925aa96e9515
Administrator <Administrator@192.168.0.100>
2018-11-27 21:45:38 +0800

    补充代码

--- src/user/Test.java
+++ src/user/Test.java
@@ -15,6 +15,16 @@ public class Test {
 				String userName=input.next();
 				System.out.println("请输入密码");
 				String userPwd=input.next();
+				DataWrap dd=m.logIn(userName, userPwd);
+				if(dd.getCode()==1) {
+					System.out.println("登录成功");
+				}else if(dd.getCode()==2) {
+					System.out.println("密码错误");
+				}else if(dd.getCode()==3) {
+					System.out.println("用户不存在");
+				}else if(dd.getCode()==4) {
+					System.out.println("账户被锁定");
+				}
 				
 			}else if(num==2) {
 				System.out.println("谢谢使用");
--- src/user/UserManager.java
+++ src/user/UserManager.java
@@ -15,6 +15,9 @@ public class UserManager {
 		super();
 		User root=new Root();
 		this.add(root);
+		
+		User u=new NormalUser("jj", "123", "123456");	
+		this.add(u);
 	}
 	/**
 	 * 在数组中添加元素
@@ -36,7 +39,7 @@ public class UserManager {
 			arr=Arrays.copyOf(arr, arr.length+arr.length>>1);
 		}
 		
-		arr[this.size++]=user;
+		this.arr[this.size++]=user;
 		return true;
 	}
 	/**
@@ -53,36 +56,76 @@ public class UserManager {
 		return null;
 	}
 	
-	/*public DataWrap logIn(String userName,String userPwd) {
-		User user=this.seek(userName);
-		if(user==null) {
-			 DataWrap wrap=new DataWrap(3,null);
-			 return wrap;
+	public DataWrap logIn(String userName,String userPwd) {
+		for (int i = 0; i < this.size; i++) {
+			if(this.arr[i].getUserName().equals(userName)) {
+				if(this.arr[i].getUserPwd().equals(userPwd)) {
+					if(this.arr[i] instanceof NormalUser) {
+						//重置登录次数
+						NormalUser n=(NormalUser)this.arr[i];
+						if(n.getStatus()==2) {
+							DataWrap wrap=new DataWrap(4,null);
+							return wrap;
+						}
+						n.setAginest(3);
+					}
+					DataWrap wrap=new DataWrap(1,this.arr[i]);
+					return wrap;
+				}else {
+					int code=2;//密码错误
+					if(this.arr[i] instanceof NormalUser) {
+						boolean uu=this.count(this.arr[i]);
+						if(!uu) {
+							code=4;//输入密码超过三次  账户被锁定
+						}
+					}					
+					DataWrap wrap=new DataWrap(code,null);
+					return wrap;
+				}
+			}
 		}
-		
-		
+		DataWrap wrap=new DataWrap(3,null);
+		return wrap;	
 	}
-	*/
-	class DataWrap{
-		private int code;//状态码  1.登陆成功  2.密码错误  3.用户不存在  4.账户被锁定
-		private User user;
-		public int getCode() {
-			return code;
+	
+	private boolean count(User user) {
+		for (int i = 0; i < this.size; i++) {
+			if(arr[i]==user) {
+				NormalUser n=(NormalUser)arr[i];
+				int left=n.getAginest();
+				if(left<=1) {
+					n.setStatus(2);
+					return false;	//账户被锁定
+				}else {
+					n.setAginest(left-1);
+				}
+				this.arr[i]=n;
+			}
 		}
-		public void setCode(int code) {
-			this.code = code;
-		}
-		public User getUser() {
-			return user;
-		}
-		public void setUser(User user) {
-			this.user = user;
-		}
-		public DataWrap(int code, User user) {
-			super();
-			this.code = code;
-			this.user = user;
-		}
-		
+		return true;
 	}
+	
+}
+
+class DataWrap{
+	private int code;//状态码  1.登陆成功  2.密码错误  3.用户不存在  4.账户被锁定
+	private User user;
+	public int getCode() {
+		return code;
+	}
+	public void setCode(int code) {
+		this.code = code;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public DataWrap(int code, User user) {
+		super();
+		this.code = code;
+		this.user = user;
+	}
+	
 }
